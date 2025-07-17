7d036c24c02a44f5db5bf888053432da69b31f82
Giuseppe Lomasto <g.lomasto.94@gmail.com>
2025-07-17 16:14:50 +0200

    prova test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import userEvent from "@testing-library/user-event";
+
+import App from "./App";
+
+// testare aggiunta componenti
+test("Aggiunta nuovo libro", () => {
+	render(<App />);
+
+	const libro = screen.getAllByRole(".list-group-item");
+
+	// recupero inputs del form obbligatori
+	const titolo = screen.getByLabelText(/titolo/i);
+	const autore = screen.getByLabelText(/autore/i);
+	const annoPubblicazione = screen.getByLabelText(/annoPubblicazione/i);
+
+	// recupero il bottone submit
+	const submit = screen.getByRole("button", { name: /Aggiungi/i });
+
+	// compilo il form
+	userEvent.type(titolo, "il signore degli anelli");
+	userEvent.type(autore, "Unknow");
+	userEvent.type(annoPubblicazione, "1234");
+
+	// al submit
+	userEvent.click(submit);
+
+	expect(libro).toBeInTheDocument();
 });
